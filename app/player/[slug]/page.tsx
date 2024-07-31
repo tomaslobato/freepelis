c677e94c847b962766ceb35b8f3248ac909c19e2
tomaslobato <tlobatodev@gmail.com>
2024-07-31 18:56:17 -0300

    switched to vidsrc.me

--- app/player/[slug]/page.tsx
+++ app/player/[slug]/page.tsx
@@ -1,5 +1,5 @@
 export default function Page({ params }: { params: { slug: string } }) {
   return (
-    <iframe src={`https://vidsrc.to/embed/movie/${params.slug}`} allowFullScreen referrerPolicy="origin" className="h-screen w-screen"></iframe>
+    <iframe src={`https://vidsrc.me/embed/movie/${params.slug}`} allowFullScreen referrerPolicy="origin" className="h-screen w-screen"></iframe>
   )
 }
